6d5584285dbcd81d6e2c4d5b5cbfa6a25ce6a9a0
KuroeNekoDemon <159578626+KuroeNekoDemon@users.noreply.github.com>
2024-04-08 17:46:55 -0400

    Update endeavouros_install.sh

--- endeavouros_install.sh
+++ endeavouros_install.sh
@@ -1,13 +1,10 @@
 #!/bin/bash
 
 #To reinstall the base components for Nvidia drivers
-sudo pacman -S --noconfirm base-devel linux-headers git nano --needed
+sudo pacman -S --noconfirm base-devel linux-headers git nano steam wine kdenlive flatpak lutris vim neovim curl wget gimp krita inkscape qbittorrent hyprshot hyprshot-gui zenity libreoffice-still sdl2 freetype harfbuzz onnxruntime
 
 #To install Nvidia drivers
 yay -S --noconfirm nvidia nvidia-utils lib32-nvidia-utils opencl-nvidia
-
-#To reinstall all of my packages I need from pacman
-sudo pacman -S --noconfirm steam wine kdenlive flatpak lutris vim neovim curl wget gimp krita inkscape qbittorrent hyprshot hyprshot-gui zenity libreoffice-still sdl2 freetype harfbuzz onnxruntime
 
 #To reinstall all the packages I need from the AUR
 yay -S --noconfirm ttf-ms-win11-auto
