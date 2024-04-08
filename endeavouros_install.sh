42e754b187e84e3e2a8f1ec68cc3fe753f9d52b1
KuroeNekoDemon <159578626+KuroeNekoDemon@users.noreply.github.com>
2024-04-08 16:37:35 -0400

    Update endeavouros_install.sh
    Code fixes

--- endeavouros_install.sh
+++ endeavouros_install.sh
@@ -6,9 +6,6 @@ sudo pacman -S --noconfirm base-devel linux-headers git nano --needed
 #To install Nvidia drivers
 yay -S --noconfirm nvidia nvidia-utils lib32-nvidia-utils opencl-nvidia
 
-#To remind me to edit Grub when this is done
-echo "Remember to add nvidia-drm.modeset=1 to the bootloader settings in grub and use sudo grub-mkconfig -o /boot/grub/grub.cfg to update Grub"
-
 #To reinstall all of my packages I need from pacman
 sudo pacman -S --noconfirm steam wine kdenlive flatpak lutris vim neovim curl wget gimp krita inkscape qbittorrent hyprshot hyprshot-gui zenity libreoffice-still sdl2 freetype harfbuzz onnxruntime
 
@@ -23,7 +20,7 @@ yay -S --noconfirm waypaper
 
 #To reinstall OBS-Studio from flathub plus the VK-Capture plugin to window capture specific windows with Pipewire
 flatpak install --user flathub com.obsproject.Studio -y
-flatpak install --user flathub com.obsproject.Studio.Plugins.OBSVkCapture -yay
+flatpak install --user flathub com.obsproject.Studio.Plugins.OBSVkCapture -y
 
 #To reinstall Hyprland, credit goes to Stephan Raabe for the amazing setup and dotfiles go to his gitlab to check out his amazing work
 cd ~/Downloads
@@ -44,3 +41,9 @@ unzip JetBrainsMono.zip -d ~/.local/share/fonts
 fc-cache ~/.local/share/fonts
 cd ~/
 git clone https://github.com/NvChad/starter ~/.config/nvim && nvim
+
+#To remind me to edit Grub when this is done
+echo "Remember to add nvidia-drm.modeset=1 to the bootloader settings in the line GRUB_CMDLINE_LINUX_DEFAULT in /etc/default/grub and use sudo grub-mkconfig -o /boot/grub/grub.cfg to update Grub"
+echo "Also reboot into KDE Plasma 6 first to make sure everything worked then Hyprland"
+echo "Add in /etc/environment as root WRL_NO_HARDWARE_CURSORS=1 to get the cursor to appear in the second monitor"
+echo "Add in ~/.config/hypr/hyprland.conf exec-once=waypaper --restore and exec-once=nm-applet --indicator at the bottom, save and reboot"
